c1b52f0acb3ed015e524967bc828285979940e88
Leo_A_M <leoahmedmushtaq@gmail.com>
2025-01-19 14:11:21 +0100

    aeration

--- Traitement_image/tstCouleur.c
+++ Traitement_image/tstCouleur.c
@@ -1,56 +1,48 @@
-// Leo AHMED MUSHTAQ
-// Ce fichier permet de tester toutes les fonctions en rapport avec la detection de couleur.
-
 #include "declaration.h"
 
-
 int main() {
-    int lignes, colonnes, d;
-    
-    // Ouverture du fichier
-    FILE* file;
-    
-    file = fopen("/Users/leo_a_m/Desktop/Cours/PFR/PFR1_G1_2024-25/Traitement_image/DATA/IMG_5389.txt", "r");
+    // Ouvrir le fichier image
+    FILE *file = fopen("/Users/leo_a_m/Desktop/Cours/PFR/PFR1_G1_2024-25/Traitement_image/DATA/IMG_5389.txt", "r");
     if (file == NULL) {
-        printf("Erreur : Impossible d'ouvrir le fichier.\n");
-        exit(1);
+        printf("Erreur d'ouverture du fichier image.\n");
+        return 1;
     }
-   
- /*####################################################################################################################*/
-    
-    // Lecture de l'image
+
+    int lignes, colonnes, d;
     readImage(file, &lignes, &colonnes, &d);
-/*
-    //Affichage des 10 premières lignes du canal Vert
-    printf("Canal Vert (IMAGE_V) :\n");
-    for (int i = 0; i < 300 && i < lignes; i++) {
-        for (int j = 0; j < colonnes; j++) {
-            printf("%d ", IMAGE_V[i][j]);
-        }
-    }
-*/
-    // Fermeture du fichier
     fclose(file);
 
-/*####################################################################################################################*/
-   
-   // Test de la fonction valeur_RVB
-    valeur_RVB(IMAGE_R, IMAGE_V, IMAGE_B);
-    printf("\n%s\n", "###################################################################################");
+    // Afficher les valeurs RVB du pixel (0,0)
     printf("Les valeurs RVB du pixel (0,0) sont : %d %d %d\n", IMAGE_R[0][0], IMAGE_V[0][0], IMAGE_B[0][0]);
-   
-/*####################################################################################################################*/
-    
+
+    // Stocker les valeurs RVB dans la structure Liste_RVB
+    valeur_RVB(IMAGE_R, IMAGE_V, IMAGE_B);
+
     // TEST DE LA FONCTION BitsPoidsForts
     int nb_bits_poids_forts = 4;
-    
+
     // Conversion des pixels en binaire
     liste_decimal_en_binaire(Tableau_RVB, nb_bits_poids_forts);
 
     // Affichage des pixels convertis en binaire
-    afficher_liste_binaire(Tableau_RVB_bin, nb_bits_poids_forts);
-    
-    
+    //afficher_liste_binaire(Tableau_RVB_bin, nb_bits_poids_forts);
+
+    // Afficher les valeurs binaires du pixel (0,0)
+    printf("Les valeurs binaires du pixel (0,0) sont :\n");
+    printf("%d\n",Tableau_RVB[0][0].R );
+    printf("R_bin: ");
+    for (int i = 0; i < nb_bits_poids_forts; i++) {
+        printf("%d", Tableau_RVB_bin[0][0].R_bin[i]);
+    }
+    printf("\nV_bin: ");
+    for (int i = 0; i < nb_bits_poids_forts; i++) {
+        printf("%d", Tableau_RVB_bin[0][0].V_bin[i]);
+    }
+    printf("\nB_bin: ");
+    for (int i = 0; i < nb_bits_poids_forts; i++) {
+        printf("%d", Tableau_RVB_bin[0][0].B_bin[i]);
+    }
+    printf("\n");
 
     return 0;
 }
